8a280159232bd5a82141f382078da0a17819e5c9
Stella-Chaves <stellachavesyoga@gmail.com>
2018-09-02 17:36:33 -0500

    cat 2 added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
+
+var cat1 = document.getElementById("catImg");
+var cat2 = document.getElementById("catImg2");
+
 var button = document.getElementById("button"),
   count = 0;
 button.onclick = function() {
   count += 1;
-  button.innerHTML = "Cat Clicked: " + count;
+  button.innerHTML = cat1 + "Cat Clicked: " + count;
+};
+
+var button2 = document.getElementById("button2"),
+  count = 0;
+button2.onclick = function() {
+  count += 1;
+  button2.innerHTML = cat1 + "Cat Clicked: " + count;
 };
