8450585a941fb9940a2a87d2ce02f5c4f18095bf
Stella-Chaves <stellachavesyoga@gmail.com>
2018-09-04 15:09:42 -0500

    moved cat img to button

--- js/app.js
+++ js/app.js
@@ -4,23 +4,16 @@ var catImgs = ['<a href="#"><img src="img/zenCat.jpg" style="width:512px;height:
 '<a href="#"><img src="img/feathersCat.jpg" style="width:500px;height:333px;/></a>', '<a href="#"><img src="img/bookCat.jpg"/></a>']
 var text = "<ul>";
 cats.forEach(catList);
-catImgs.forEach(catPhotos);
 
 function catList(value) {
-  text += "<li>" + value + "</li>";
-}
-function catPhotos(value) {
   text += "<li>" + value + "</li>";
 }
 
 document.getElementById("demo").innerHTML = text;
 
-var cat1 = document.getElementById("catImg");
-var cat2 = document.getElementById("catImg2");
-
 var button = document.getElementById("button"),
   count = 0;
 button.onclick = function() {
   count += 1;
-  button.innerHTML = cat1 + "Cat Clicked: " + count;
+  button.innerHTML = catImgs + "Cat Clicked: " + count;
 };
