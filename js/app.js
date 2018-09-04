2ad1ec4fcf361dbdc49aa681e10746f61af6dd4a
Stella-Chaves <stellachavesyoga@gmail.com>
2018-09-04 14:33:46 -0500

    list and photos added to array

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+var cats = ["Zen Cat", "Yarn Cat", "Bench Cat", "Feathers Cat", "Book Cat"];
+var catImgs = ['<a href="#"><img src="img/zenCat.jpg" style="width:512px;height:341px;/></a>',
+'<a href="#"><img src="img/yarnCat.jpg" style="width:512px;height:341px;/></a>', '<a href="#"><img src="img/benchCat.jpg" style="width:512px;height:341px;/></a>',
+'<a href="#"><img src="img/feathersCat.jpg" style="width:500px;height:333px;/></a>', '<a href="#"><img src="img/bookCat.jpg"/></a>']
+var text = "<ul>";
+cats.forEach(catList);
+catImgs.forEach(catPhotos);
+
+function catList(value) {
+  text += "<li>" + value + "</li>";
+}
+function catPhotos(value) {
+  text += "<li>" + value + "</li>";
+}
+
+document.getElementById("demo").innerHTML = text;
 
 var cat1 = document.getElementById("catImg");
 var cat2 = document.getElementById("catImg2");
