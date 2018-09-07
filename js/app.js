50ad8b4740b6125858e9b9018bd0436608cf7420
Stella-Chaves <stellachavesyoga@gmail.com>
2018-09-07 17:13:58 -0500

    view functions

--- js/app.js
+++ js/app.js
@@ -63,6 +63,67 @@ var octopus = {
   }
 };
 
+/* View */
+
+var catView = {
+
+  init: function() {
+    // store pointer to our DOM elements 
+    this.catElem = document.getElementById('cat');
+    this.catNameElem = document.getElementById('cat-name');
+    this.catImageElem = document.getElementById('cat-img');
+    this.countElem = document.getElementById('cat-count');
+
+    // on click, increment the current cat's counter
+    this.catImageElem.addEventListener('click', function(){
+      octopus.incrementCounter();
+    });
+
+    this.render();
+  },
+
+  render: function() {
+    //update DOM elements
+    var currentCat = octopus.getCurrentCat();
+    this.countElem.textContent = currentCat.clickCount;
+    this.catNameElem.textContent = currentCat.name;
+    this.catImageElem.src = currentCat.imgSrc;
+  }
+};
+
+var catListView = {
+
+  init: function() {
+    var cat, elem, i;
+    var cats = octopus.getCats();
+
+    //empty the cat list
+    this.catListElem.innerHTML = '';
+
+    // loop over the cats
+    for (i = 0; i < cats.length; i++) {
+      cat = cats[i];
+
+    // make a new cat list item and set its text
+    elem = document.createElement('li');
+    elem.textContent = cat.name;
+
+    // on click, setCurrentCat and render catView
+    elem.addEventListener('click', (function(catCopy) {
+      return function() {
+        octopus.setCurrentCat(catCopy);
+        catView.render();
+      };
+    })(cat));
+
+    //add element to the list
+    this.catListElem.appendChild(elem);
+    }
+  }
+}; 
+
+octopus.init();
+
 var text = "<ul>";
 model.forEach(catList);
 
